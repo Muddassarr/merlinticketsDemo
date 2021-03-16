514bb4ab2ca976638c32066fb58a5ca85db00820
Muddassar Riaz <muddassar@merlinsoft.co.uk>
2021-03-16 12:50:14 +0000

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,148 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
+
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+    $('#navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+	
+
+	/*---------------------------------------------*
+     * Menu Background Change
+     ---------------------------------------------*/
+	
+	var windowWidth = $(window).width();
+    if (windowWidth > 757) {
+
+        
+          
+            $(window).scroll(function () {
+                if ($(this).scrollTop() >200) {
+                    $('.navbar').fadeIn(200);
+                    $('.navbar').addClass('menu-bg');
+
+                } else {
+                    
+                    $('.navbar').removeClass('menu-bg');
+                }
+            });
+        
+    }
+		$('#bs-example-navbar-collapse-1').localScroll();
+		
+	/*---------------------------------------------*
+     * Scroll Up
+     ---------------------------------------------*/	
+		$(window).scroll(function(){
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+		});
+		
+		$('.scrollup').click(function(){
+			$("html, body").animate({ scrollTop: 0 },1000);
+			return false;
+		});
+		
+	
+
+
+    /*---------------------------------------------*
+     * STICKY scroll
+     ---------------------------------------------*/
+
+		$.localScroll();
+
+
+
+    /*---------------------------------------------*
+     * Counter 
+     ---------------------------------------------*/
+
+//    $('.statistic-counter').counterUp({
+//        delay: 10,
+//        time: 2000
+//    });
+
+
+
+
+    /*---------------------------------------------*
+     * WOW
+     ---------------------------------------------*/
+
+//        var wow = new WOW({
+//            mobile: false // trigger animations on mobile devices (default is true)
+//        });
+//        wow.init();
+
+
+    /* ---------------------------------------------------------------------
+     Carousel
+     ---------------------------------------------------------------------= */
+
+//    $('.testimonials').owlCarousel({
+//        responsiveClass: true,
+//        autoplay: false,
+//        items: 1,
+//        loop: true,
+//        dots: true,
+//        autoplayHoverPause: true
+//
+//    });
+
+
+    //End
+});
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+function popupfucntion(){
+    alert("TO OPEN THIS FILE AFTER DOWNLAODING:                                         Go to Download folder -> Right Click on merlin_demoV69.1.zip file -> Select Extract all -> Click Extract");
+}
